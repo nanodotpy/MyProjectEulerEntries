7b1d7435af7069bbd3373caece2e6af0cb90a30e
nano.py <46073902+nanodotpy@users.noreply.github.com>
2019-01-16 20:19:27 +0100

    5 % difficulty

--- Problem 23.py
+++ Problem 23.py
@@ -15,38 +15,22 @@ def abundant(number):
         return True
     else :
         return False
+
+somme = 0
+
+abundants = [value for value in range(1,28124) if abundant(value)]
     
-
-abundants = []    
+abundant_set = set(abundants)        
     
-sumabundants = []
+for a in range(1,28124):
+    for i in abundant_set:
+        if i<a and a - i in abundant_set:
+            break
+    else :
+        somme += a
+            
+print(somme)
+        
+        
+            
 
-for a in range(1,30000):
-    if abundant(a) :
-        abundants.append(a)
-        
-def sum_of_abundants(number) :
-    
-    for i in abundants :
-        if i <= number :
-            pair = number - i 
-            if pair in abundants :
-                return True
-            else : 
-                return False
-        
-        
-for number in range(1,28124) :
-    if sum_of_abundants(number) :
-        sumabundants.append(number)
-       
-print(sumabundants)        
-        
-result = [value for value in list(range(1,28124)) if value not in sumabundants]
-
-print(sum(result))
-        
-
-   
-
-                  
