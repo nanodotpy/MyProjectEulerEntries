2a326ed743c5b7ed7ef9e9c8349109b7b21a801c
nano.py <46073902+nanodotpy@users.noreply.github.com>
2019-02-23 11:33:27 +0100

    15% difficulty

---
+++ Problem 73.py
@@ -0,0 +1,16 @@
+import fractions
+import math
+
+setincroyable = set()
+
+for d in range(1, 12001):
+    print(d)
+    for n in range(int(math.ceil(1/3*d)), int(math.floor(1/2*d)+1)):
+        if math.gcd(d, n) == 1:
+            setincroyable.add(fractions.Fraction(n,d))
+            
+setincroyable -= {fractions.Fraction(1, 2), fractions.Fraction(1, 3)}
+
+print(len(setincroyable))
+
+#Took 3 mins but NICE
