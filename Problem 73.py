1eb91a85cecce65db2d7d25cf3c7de0a21fac354
nano.py <46073902+nanodotpy@users.noreply.github.com>
2019-02-23 13:19:23 +0100

    15% difficulty

--- Problem 73.py
+++ Problem 73.py
@@ -4,7 +4,6 @@ import math
 setincroyable = set()
 
 for d in range(1, 12001):
-    print(d)
     for n in range(int(math.ceil(1/3*d)), int(math.floor(1/2*d)+1)):
         if math.gcd(d, n) == 1:
             setincroyable.add(fractions.Fraction(n,d))
